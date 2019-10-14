cba8a518026ab8d42d721119c1fee518ca05e654
Matthew Wardle <mwardle.22@gmail.com>
2019-10-14 18:07:45 +0000

    Modify assertEqual.js using template literals

--- assertEqual.js
+++ assertEqual.js
@@ -1,6 +1,6 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
-  let result = (actual === expected ? "✅✅✅ Assertion Passed: " + actual + " === " + expected : "❌❌❌ Assertion Failed: " + actual + " !== " + expected);
+  let result = (actual === expected ? `✅✅✅ Assertion Passed: ${actual} === ${expected}.` : `❌❌❌ Assertion Failed: ${actual} !== ${expected}.`);
   console.log(result);
 };
 
