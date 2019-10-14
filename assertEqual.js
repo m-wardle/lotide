3d7b58e9495def06f77a8d8775d783d4cdc7add7
Matthew Wardle <mwardle.22@gmail.com>
2019-10-14 17:46:51 +0000

    Create assertEqual function to colourfully check whether two arguments are equal

---
+++ assertEqual.js
@@ -0,0 +1,13 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  let result = (actual === expected ? "✅✅✅ Assertion Passed: " + actual + " === " + expected : "❌❌❌ Assertion Failed: " + actual + " !== " + expected);
+  console.log(result);
+};
+
+// TEST CODE
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("TesT", "Test");
+assertEqual("TesT", "TesT");
+assertEqual(1, 2);
