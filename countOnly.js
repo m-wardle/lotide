63ae02a9be08db74fda23c55351caec3bc80e689
Matthew Wardle <mwardle.22@gmail.com>
2019-10-15 23:43:13 +0000

    Create function which takes a collection of items, and returns a count of specific items from that collection

---
+++ countOnly.js
@@ -0,0 +1,46 @@
+// Test/Assertion Functions
+
+const assertEqual = function(actual, expected) {
+  let result = (actual === expected ? `✅✅✅ Assertion Passed: ${actual} === ${expected}.` : `❌❌❌ Assertion Failed: ${actual} !== ${expected}.`);
+  console.log(result);
+};
+
+// countOnly Function
+
+// allItems: an array of strings that we need to look through
+// itemsToCount: an object specifying what to count
+const countOnly = function(allItems, itemsToCount) {
+const results = {};
+
+for (const item of allItems) {
+   if (itemsToCount[item]) { 
+    if (results[item]) {
+      results[item] += 1;
+    } else {
+      results[item] = 1;
+    }
+  }
+}
+console.log(results)
+return results;
+}
+
+// Test Cases
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true });
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
