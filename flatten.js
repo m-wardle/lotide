3d45ce9ff3a610f42c33176cff3612fcedb3faf2
Matthew Wardle <mwardle.22@gmail.com>
2019-10-15 19:16:12 +0000

    Create function which, given an array with other arrays inside it, can flatten into a single level array

---
+++ flatten.js
@@ -0,0 +1,44 @@
+// Test/Assertion Functions
+
+const eqArrays = function(arrayOne, arrayTwo) {
+  if (arrayOne.length !== arrayTwo.length) {
+    return false;
+  }
+  
+  for (let i = 0; i < arrayOne.length; i++) {
+    if (arrayOne[i] !== arrayTwo[i]) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+const assertArraysEqual = function(arrayOne, arrayTwo) {
+  let result = (eqArrays(arrayOne, arrayTwo) ? `✅✅✅ Assertion Passed: ${arrayOne} === ${arrayTwo}.` : `❌❌❌ Assertion Failed: ${arrayOne} !== ${arrayTwo}.`);
+  console.log(result);
+};
+
+// Flatten Function
+
+const flatten = function(array) {
+  for (i = 0; i < array.length;) {
+    if (Array.isArray(array[i]) === true) {
+      let counter = 0;
+      for (element of array[i]) {
+        array.splice(i + counter, 0, element);
+        counter++
+      }
+      array.splice(i + counter, 1);
+      i += counter;
+    } else {
+      i++
+    }
+  }
+
+  return array;
+}
+
+assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6]);
+assertArraysEqual(flatten([22, -1, [-24, 44, 891], [23, -111], 3]), [22, -1, -24, 44, 891, 23, -111, 3]);
+assertArraysEqual(flatten(['test', ['hello', 'test2'], 2, 4, [24, 42]]), ['test', 'hello', 'test2', 2, 4, 24, 42])
