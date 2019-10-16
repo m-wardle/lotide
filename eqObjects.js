143531da752a5190255a3bc84950d5a220e5d767
Matthew Wardle <mwardle.22@gmail.com>
2019-10-16 17:30:06 +0000

    Create function to test if two objects are equal, so long as those objects do not contain other objects.

---
+++ eqObjects.js
@@ -0,0 +1,58 @@
+// Assertion/Test Functions
+
+const assertEqual = function(actual, expected) {
+  let result = (actual === expected ? `✅✅✅ Assertion Passed: ${actual} === ${expected}.` : `❌❌❌ Assertion Failed: ${actual} !== ${expected}.`);
+  console.log(result);
+};
+
+const eqArrays = function(arrayOne, arrayTwo) {
+  if (arrayOne.length !== arrayTwo.length) {
+    return false;
+  }
+  
+  for (let i = 0; i < arrayOne.length; i++) {
+    if (arrayOne[i] !== arrayTwo[i]) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+// Object Equality Assertion Function
+
+const eqObjects = function(obj1, obj2) {
+  let keys1 = Object.keys(obj1);
+  let keys2 = Object.keys(obj2);
+
+  if (keys1.length > keys2.length || keys2.length > keys1.length) {
+    return false
+  } else {
+    for (let key of keys1) {
+      console.log(obj1[key], obj2[key])
+      if (!obj2[key]) {
+        console.log("didn't match");
+        return false
+      } else if ((Array.isArray(obj1[key]) && Array.isArray(obj2[key])) ? !eqArrays(obj1[key], obj2[key]) : obj1[key] !== obj2[key]) {
+        return false;
+      }
+    }
+    return true
+  }
+}
+
+// Test Cases
+
+const ab = { a: "1", b: "2" };
+const ba = { b: "2", a: "1" };
+assertEqual(eqObjects(ab, ba), true)
+
+const abc = { a: "1", b: "2", c: "3" };
+assertEqual(eqObjects(ab, abc), false);
+
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+assertEqual(eqObjects(cd, dc), true);
+
+const cd2 = { c: "1", d: ["2", 3, 4] };
+assertEqual(eqObjects(cd, cd2), false);
