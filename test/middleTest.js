e22c460cdbbaaa8370e8707cd4a65132fa978ce4
Matthew Wardle <mwardle.22@gmail.com>
2019-10-21 17:22:20 +0000

    refactor middleTest to use Mocha/Chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,7 +1,20 @@
 const middle = require('../middle');
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([1, 2]), []);
-assertArraysEqual(middle([1]), []);
+describe("#middle", () => {
+  it("returns [3, 4] for [1, 2, 3, 4, 5, 6]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+  });
+
+  it("returns [3] for [1, 2, 3, 4, 5]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]);
+  });
+
+  it("returns an empty array if array only contains one item", () => {
+    assert.deepEqual(middle([1]), []);
+  });
+
+  it("returns an empty array if input contains only two items", () => {
+    assert.deepEqual(middle([1, 2]), []);
+  });
+});
