b51e1298f5ddaa173d753b036279d76a4141ea0f
Matthew Wardle <mwardle.22@gmail.com>
2019-10-21 16:34:57 +0000

    Fix minor formatting errors

--- test/assertArraysEqualTest.js
+++ test/assertArraysEqualTest.js
@@ -1,8 +1,7 @@
-const eqArrays = require('../eqArrays');
 const assertArraysEqual = require('../assertArraysEqual');
 
 assertArraysEqual([1, 2, 3], [1, 2, 3]);
 assertArraysEqual([1, 2, '3'], [1, 2, 3]);
-assertArraysEqual([1, 2, 3], [1, 2, 3, 4])
-assertArraysEqual([1, 2, 3], [1, '2', '3'])
-assertArraysEqual([4, -27, 333], [4, -27, 333])
+assertArraysEqual([1, 2, 3], [1, 2, 3, 4]);
+assertArraysEqual([1, 2, 3], [1, '2', '3']);
+assertArraysEqual([4, -27, 333], [4, -27, 333]);
